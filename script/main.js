6784c6840e072fd8782576e44fb431c55d600991
PHAGE-Official <100456211+PHAGE-Official@users.noreply.github.com>
2022-08-31 18:29:46 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,13 @@
+/*
+Main.js
+--------------------------------------------------
+Author:NLTech Coporation & Phage Team
+Licence:PSSPL
+Time:2022/8/31/18:25
+Version:0.0.1_Dev_Alpha
+--------------------------------------------------
+Todo List:
+	Not Decide yet
+
+*/
+window.alert('Welcome!');
