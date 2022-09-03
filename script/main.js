5980f0b0442818c99e42a56d616d75fbf28c4fde
PHAGE-Official <100456211+PHAGE-Official@users.noreply.github.com>
2022-09-03 10:29:08 +0800

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -3,13 +3,17 @@ Main.js
 --------------------------------------------------
 Author:NLTech Coporation & Phage Team
 Licence:PSSPL
-Time:2022/09/01/22:02
+Time:2022/09/03/10:21
 Version:0.0.1_Dev_Alpha
 --------------------------------------------------
 Todo List:
-	Not Decide yet
+	Search Function Is Needed To Search
+	Keywords In The SQL|Database
 
 */
+
+// Variables
+let Flag = true;
 
 function Get_Time() {
 	// Get Current Time
@@ -30,10 +34,23 @@ function Exit() {
 
 function About(){
 
-	let info = 'This Website Is Developed By Phage Team';
-	// Get The Body Element
-	let body = document.getElementsByTagName('body')[0];
-	let div = document.createElement('div');
-	body.appendChild(div);
-	div.innerHTML = info;
+	/*
+	This Function Show The Information About This Website
+	*/
+	if (Flag == true){
+		let info = 'This Website Is Developed By Phage Team.Version:0.0.1_Dev_Alpha';
+		// Get The Body Element
+		let body = document.getElementsByTagName('body')[0];
+		let div = document.createElement('div');
+		body.appendChild(div);
+		div.innerHTML = info;
+		div.setAttribute('id','info');
+		Flag = false;
+	}
+	// If Existed Then Delete It
+	else{
+		let div = document.getElementById('info');
+		div.parentNode.removeChild(div);
+		Flag = true;
+	}
 }
